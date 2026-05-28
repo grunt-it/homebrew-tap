e2d14aa634f28468bb09b9702b4a4ed1c56edc18
Nik Divjak <divjak.nik@gmail.com>
2026-05-28 22:06:09 +0200

    bump gruntcode to 1.15.10-grunt.8

--- Formula/gruntcode.rb
+++ Formula/gruntcode.rb
@@ -6,23 +6,23 @@ class Gruntcode < Formula
   desc "grunt-it soft-fork of opencode with hivemind-native autonomous-coordinator patches"
   homepage "https://github.com/grunt-it/gruntcode"
   license "MIT"
-  version "1.15.10-grunt.6"
+  version "1.15.10-grunt.8"
 
   on_macos do
     on_arm do
-      url "https://github.com/grunt-it/gruntcode/releases/download/v1.15.10-grunt.6/gruntcode-darwin-arm64.tar.gz"
-      sha256 "840810aadbe32aab4d28d3f677591f653546778b7753a34cb4fc27120f4e152d"
+      url "https://github.com/grunt-it/gruntcode/releases/download/v1.15.10-grunt.8/gruntcode-darwin-arm64.tar.gz"
+      sha256 "e61a87bdc389520ecd43355447f80e270198eabaa86c0c35868a5be322d8fe1c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/grunt-it/gruntcode/releases/download/v1.15.10-grunt.6/gruntcode-linux-arm64.tar.gz"
-      sha256 "41acbca04459b6990c2981d1b2e4a755639f3ab38030e48e69d90f333e72c3e4"
+      url "https://github.com/grunt-it/gruntcode/releases/download/v1.15.10-grunt.8/gruntcode-linux-arm64.tar.gz"
+      sha256 "78d54764f9f66ce432388b1bf056ff3e7d4d1d5392dd97dfcdd3e5a043f2ae00"
     end
     on_intel do
-      url "https://github.com/grunt-it/gruntcode/releases/download/v1.15.10-grunt.6/gruntcode-linux-x64.tar.gz"
-      sha256 "75f1951855fae4984dc3a77ec8060b292a87efc3e06404cc96f0fde6dd9157d9"
+      url "https://github.com/grunt-it/gruntcode/releases/download/v1.15.10-grunt.8/gruntcode-linux-x64.tar.gz"
+      sha256 "5e6f9269b19ce67ec02a4ba4cd52f36afae1682eb7dd1f7c92a5cf46780f5cad"
     end
   end
 
